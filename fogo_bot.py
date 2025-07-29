43845b091341c70c2b174c8979c4d9cbb18c7645
vietbodoi09 <hviet09072007@gmail.com>
2025-07-29 13:42:55 +0700

    Update fogo_bot.py

--- fogo_bot.py
+++ fogo_bot.py
@@ -191,4 +191,12 @@ async def main():
     await app.run_polling()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except RuntimeError as e:
+        if "event loop is already running" in str(e):
+            import nest_asyncio
+            nest_asyncio.apply()
+            asyncio.get_event_loop().run_until_complete(main())
+        else:
+            raise
